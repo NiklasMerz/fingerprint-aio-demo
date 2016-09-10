f341e281d7112f041dff255be9a593bf85939724
Niklas Merz <NiklasMerz@gmx.net>
2016-09-10 09:32:34 +0200

    Show button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,25 +2,41 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $ionicPlatform) {
 
-$ionicPlatform.ready(function() {
-  //###################### Fingerprint Plugin Test ###############################
-  Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
+  $ionicPlatform.ready(function() {
+    //###################### Fingerprint Plugin Test ###############################
+    Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
+    $scope.available = "Not checked";
 
-  function successCallback(){
-    alert("Authentication successfull");
-  }
+    function successCallback(){
+      alert("Authentication successfull");
+    }
 
-  function errorCallback(){
-    alert("Authentication invalid");
-  }
+    function errorCallback(err){
+      alert("Authentication invalid" + err);
+    }
 
-  function isAvailableSuccess(result) {
-    $scope.available = "Fingerprint available";
-    console.log("Is available callback triggered", result);
+    function isAvailableSuccess(result) {function successCallback(){
+      alert("Authentication successfull");
+    }
+
+    function errorCallback(){
+      alert("Authentication invalid");
+    }
+
+    function isAvailableSuccess(result) {
+      $scope.available = "Fingerprint available";
+      console.log("Is available callback triggered", result);
       Fingerprint.show({
         clientId: "Fingerprint-Demo",
         clientSecret: "password"
       }, successCallback, errorCallback);
+    }
+    $scope.available = "Fingerprint available";
+    console.log("Is available callback triggered", result);
+    Fingerprint.show({
+      clientId: "Fingerprint-Demo",
+      clientSecret: "password"
+    }, successCallback, errorCallback);
   }
 
   function isAvailableError(message) {
@@ -29,5 +45,22 @@ $ionicPlatform.ready(function() {
   }
   //##################### Fingerprint Plugin Test End#############################
 });
+
+$scope.showAuth = function(){
+  function successCallback(){
+    alert("Authentication successfull");
+  }
+
+  function errorCallback(err){
+    console.log(err);
+    alert("Authentication invalid");
+  }
+
+  Fingerprint.show({
+    clientId: "Fingerprint-Demo",
+    clientSecret: "password"
+  }, successCallback, errorCallback);
+
+};
 
 });
