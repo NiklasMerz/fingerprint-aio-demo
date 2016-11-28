d35134bbc961e9ec1f65dfcc6dba4a4a9fef7fe6
Niklas <nm4500@rhoen.de>
2016-11-28 19:24:59 +0100

    Add call

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FingerprintAIO } from 'ionic-native'
 
 import { NavController } from 'ionic-angular';
 
@@ -10,6 +11,22 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  check(){
+    FingerprintAIO.isAvailable().then(result =>{
+      alert(result);
+    }).catch(err => {
+      alert(err);
+    });
+  }
+
+  show(){
+    FingerprintAIO.show().then(result => {
+      alert(result);
+    }).catch(err => {
+      alert(err);
+    });
   }
 
 }
